ab88ce003cf2b7296b2233951ad2f14ef9dfd8c7
GitHub Action <action@github.com>
2022-09-07 23:01:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/patrickfv/horizonjetbrainside/MyBundle.kt
+++ src/main/kotlin/com/github/patrickfv/horizonjetbrainside/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.patrickfv.horizonjetbrainside
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/patrickfv/horizonjetbrainside/services/MyProjectService.kt
+++ src/main/kotlin/com/github/patrickfv/horizonjetbrainside/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.patrickfv.horizonjetbrainside.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.patrickfv.horizonjetbrainside.MyBundle
 
 class MyProjectService(project: Project) {
 
